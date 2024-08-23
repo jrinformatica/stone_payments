81a86f8510c8a0d9ba8a623b60609b79c9f49fbf
Eduardo Dadalt Andres <eduardodadalt1@gmail.com>
2024-08-23 17:15:01 -0300

    feat: Fix bug in MyResult class causing duplicate success/error/notImplemented calls

--- android/src/main/kotlin/dev/ltag/stone_payments/model/MyResult.kt
+++ android/src/main/kotlin/dev/ltag/stone_payments/model/MyResult.kt
@@ -7,24 +7,32 @@ import io.flutter.plugin.common.MethodChannel
 class MyResult(private val result: MethodChannel.Result) : MethodChannel.Result {
     private val tag = "MyResult"
     private var alreadyReply = false
+    private val lock = Object()
+
     override fun success(value: Any?) {
         Log.d(tag, "success: $value")
-        if (alreadyReply) return
-        alreadyReply = true
+        synchronized(lock) {
+            if (alreadyReply) return
+            alreadyReply = true
+        }
         result.success(value)
     }
 
     override fun error(errorCode: String, errorMessage: String?, errorDetails: Any?) {
         Log.d(tag, "error: $errorCode, $errorMessage, $errorDetails")
-        if (alreadyReply) return
-        alreadyReply = true
+        synchronized(lock) {
+            if (alreadyReply) return
+            alreadyReply = true
+        }
         result.error(errorCode, errorMessage, errorDetails)
     }
 
     override fun notImplemented() {
         Log.d(tag, "notImplemented")
-        if (alreadyReply) return
-        alreadyReply = true
+        synchronized(lock) {
+            if (alreadyReply) return
+            alreadyReply = true
+        }
         result.notImplemented()
     }
 
